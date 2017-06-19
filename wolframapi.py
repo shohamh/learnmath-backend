f53d6ee75eb02b5bc864f839b69945c8c75b75ea
shohamh <shohamh@hotmail.com>
2017-06-19 12:53:44 +0300

    Using the API kinda correctly

---
+++ wolframapi.py
@@ -0,0 +1,13 @@
+import wolframalpha
+
+app_id="LH259U-2X7QT3WQP4"
+
+client = wolframalpha.Client(app_id)
+
+query = input("Query: ")
+
+res = client.query(query)
+
+for pod in res.pods:
+    for subpod in pod.subpods:
+        print subpod.plaintext
