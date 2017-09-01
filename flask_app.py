37fe47cc04284a073939e91a88f37458e5868a6d
matanfidel <matanfid@walla.co.il>
2017-09-01 12:43:55 +0300

    Updated with comments

--- flask_app.py
+++ flask_app.py
@@ -11,9 +11,11 @@ from flask_cors import CORS, cross_origin
 DATABASE = 'db.db'
 app = Flask(__name__)
 CORS(app)
-#---------------------------------------------------------------------
-#Function that prepers the DataBase in order to execute a query.
-#---------------------------------------------------------------------
+
+
+# ---------------------------------------------------------------------
+# Function that prepers the DataBase in order to execute a query.
+# ---------------------------------------------------------------------
 def get_db():
     db = getattr(_app_ctx_stack.top, '_database', None)
     if db is None:
@@ -22,49 +24,57 @@ def get_db():
     db.row_factory = sqlite3.Row
     return db
 
-#-----------------------------------------------
-#Function that closes a connection to DataBase.
-#-----------------------------------------------
+
+# -----------------------------------------------
+# Function that closes a connection to DataBase.
+# -----------------------------------------------
 @app.teardown_appcontext
 def close_connection(exception):
     db = getattr(_app_ctx_stack.top, '_database', None)
     if db is not None:
         db.close()
 
-#---------------------------------------------------------------
-#Function that convert the result of a query into a dictionary.
-#---------------------------------------------------------------
+
+# ---------------------------------------------------------------
+# Function that convert the result of a query into a dictionary.
+# ---------------------------------------------------------------
 def make_dicts(cursor, row):
     return dict((cursor.description[idx][0], value)
                 for idx, value in enumerate(row))
 
-#-------------------------------------------------------------------------
-#Function that executes a query and returns the result(might be empty).
-#-------------------------------------------------------------------------
+
+# -------------------------------------------------------------------------
+# Function that executes a query and returns the result(might be empty).
+# -------------------------------------------------------------------------
 def query_db(query, args=(), one=False):
     cur = get_db().execute(query, args)
     rv = cur.fetchall()
     cur.close()
     return (rv[0] if rv else None) if one else rv
-#-----------------------------------------------------------------------------
-#Function that checks the validity of an email.
-#This function gets an email string as an input and returns a boolean value.
-#-----------------------------------------------------------------------------
+
+
+# -----------------------------------------------------------------------------
+# Function that checks the validity of an email.
+# This function gets an email string as an input and returns a boolean value.
+# -----------------------------------------------------------------------------
 def checkmail(email):
     EMAIL_REGEX = re.compile(r"\w+[.|\w]\w+@\w+[.]\w+[.|\w+]\w+")
     if not EMAIL_REGEX.match(email):
         return False
     return True
-#-----------------------------------------------------------------------------
-#Function that checks if an email is unique.
-#This function gets an email string as an input,and returns a boolean value.
-#-----------------------------------------------------------------------------
+
+
+# -----------------------------------------------------------------------------
+# Function that checks if an email is unique.
+# This function gets an email string as an input,and returns a boolean value.
+# -----------------------------------------------------------------------------
 def is_unique_email(email):
- cur=query_db('SELECT salt FROM users WHERE email=?', (email,))
- if len(cur)>0:
-  return False
- else:
-      return True
+    cur = query_db('SELECT salt FROM users WHERE email=?', (email,))
+    if len(cur) > 0:
+        return False
+    else:
+        return True
+
 
 # @app.route('/DBcheck')
 # @cross_origin()
@@ -89,10 +99,10 @@ def homepage():
 
 @app.route('/register', methods=["POST", "GET"])
 @cross_origin()
-#-------------------------------------------------------------------------------------------------
-#Function that adds a new user to the system.
-#This function returns an object that shows wheter the user succeeded in registration or not.
-#-------------------------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------------------------
+# Function that adds a new user to the system.
+# This function returns an object that shows wheter the user succeeded in registration or not.
+# -------------------------------------------------------------------------------------------------
 def register():
     result = {
         "success": False,
@@ -119,24 +129,23 @@ def register():
         get_db().commit()
 
         if cursor is None:
-         result["error_messages"].append("Failed to register, the username/email may be taken.")
-         cursor.close()
+            result["error_messages"].append("Failed to register, the username/email may be taken.")
+            cursor.close()
         else:
             result["success"] = True
 
     else:
         result["error_messages"].append("Email not valid.")
 
-
     return jsonify(result)
 
 
 @app.route('/login', methods=["POST", "GET"])
 @cross_origin()
-#--------------------------------------------------------------------------------
-#Function that checks if the username and password are existed in the database.
-#This function returns an object that shows wheter the user exist or not.
-#--------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------
+# Function that checks if the username and password are existed in the database.
+# This function returns an object that shows wheter the user exist or not.
+# --------------------------------------------------------------------------------
 def login():
     result = {
         "success": False,
@@ -145,11 +154,12 @@ def login():
     logindata = request.get_json(force=True)
     username = logindata["username"]
     password = logindata["password"]
-    salt_rows = query_db('SELECT salt FROM users WHERE username=?', (username, ))
-    if salt_rows and len(salt_rows)>0:
-        salt = salt_rows[0]['salt'] #try and catch sqlite3.IntegrityError: UNIQUE constraint failed: users.email
+    salt_rows = query_db('SELECT salt FROM users WHERE username=?', (username,))
+    if salt_rows and len(salt_rows) > 0:
+        salt = salt_rows[0]['salt']  # try and catch sqlite3.IntegrityError: UNIQUE constraint failed: users.email
         query = query_db('SELECT * FROM users WHERE username=? AND password=?',
-                         [username.encode("utf_8"), hashlib.pbkdf2_hmac('sha256', password.encode("utf_8"), salt, 100000)])
+                         [username.encode("utf_8"),
+                          hashlib.pbkdf2_hmac('sha256', password.encode("utf_8"), salt, 100000)])
         if query is None:
 
             result["error_messages"].append("Your username or password were incorrect.")
@@ -159,17 +169,27 @@ def login():
         else:
 
             session_key = uuid.uuid1()
-            get_db().execute('DELETE from sessions where username=?',[username,])
+            get_db().execute('DELETE FROM sessions WHERE username=?', [username, ])
             get_db().commit()
-            get_db().execute('INSERT into sessions values(?,?,?,?)',[username,session_key,datetime.datetime.utcnow().isoformat(),30])
+            get_db().execute('INSERT INTO sessions VALUES(?,?,?,?)',
+                             [username, session_key, datetime.datetime.utcnow().isoformat(), 30])
             get_db().commit()
             result["session_key"] = session_key
-
 
             result["success"] = True
     else:
         result["error_messages"].append("Username does not exist.")
 
+    return jsonify(result)
+
+
+@app.route('/question', methods=["POST", "GET"])
+def question():
+    result = {
+        "success": True,
+        "error_messages": [],
+        "problem": ""
+    }
     return jsonify(result)
 
 
